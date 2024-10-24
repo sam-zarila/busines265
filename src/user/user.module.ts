acbcd91066a7792b0f81433e24e53c930b799d05
sam zarila <zarilatroxx99@gmail.com>
2024-10-24 23:22:13 +0200

    user register

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 
 @Module({
+  imports:[
+    PassportModule,
+    JwtModule.register({
+      secret:'sam265',
+      signOptions: { expiresIn: '1h' },
+
+    }),
+    TypeOrmModule.forFeature([User])
+  ],
   controllers: [UserController],
   providers: [UserService],
 })
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 
 
 @Injectable()
 export class UserService {
+  private readonly saltRounds=10;
+  private readonly jwwSecret='sam265'
+
+  constructor(
+    @InjectRepository(User) private readonly  UserRepository:Repository<User>
+  ){  }
+
+    async createUser(name:string, email:string,password:string){
+
+      const hashedPassword = await bcrypt.hash(password, this.saltRounds);
+      const newUser = this.UserRepository.create({name, email, password: hashedPassword});
+      return await this.UserRepository.save(newUser);
+    }
+
+    async SignIn(){
+      const newUser = await this .UserRepository.findOne({where:{name}})
+
+      if (condition) {
+        
+      }
+    }
  
 }
