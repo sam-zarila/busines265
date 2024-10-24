d9d906f5db4e552fa959446512efeca601457e14
sam zarila <zarilatroxx99@gmail.com>
2024-10-25 00:13:54 +0200

    user register

--- src/DTOs/addToCart.DTO.ts
+++ src/DTOs/addToCart.DTO.ts
@@ -1,34 +1,26 @@
-import { ApiProperty } from "@nestjs/swagger"
+import { ApiProperty } from '@nestjs/swagger';
 
+export class addToCartDto {
+  @ApiProperty({ description: 'Item identifier' })
+  item: number;
 
-export class addToCartDto{
+  @ApiProperty({ description: 'Quantity of the item' })
+  quantity: number;
 
-  
-    @ApiProperty({description:' item identifier'})
-     item:number
+  @ApiProperty({ description: 'Image of the item' })
+  image: string;
 
-     @ApiProperty({description:'quantity  of the items'})
-     quantity:number
+  @ApiProperty({ description: 'Name of the item' })
+  name: string;
 
-    @ApiProperty({description:'image of the item'})
-    
-    image:string
-    
-    @ApiProperty({description:'description of the item'})
-    name:string
+  @ApiProperty({ description: 'Provider of the item' })
+  provider: string;
 
-    @ApiProperty({description:'description of the item'})
-    
-    provider:string
+  @ApiProperty({ description: 'Price of the item' })
+  price: string;
 
-    @ApiProperty({description:'price of the item'})
-    
-    price:string
+  @ApiProperty({ description: 'Location of the item' })
+  location: string;
 
-    @ApiProperty({description:'image of the item'})
-    
-
-    location:string
-  
-
-}
+  @ApiProperty({ description: 'User ID to link the cart item' })
+  userId: number; 
